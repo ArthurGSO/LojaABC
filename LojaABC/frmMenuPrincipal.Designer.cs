e0176ced6a20b56d0059642adb3717f7652dc8d7
ArthurGSO <arthurprobr422@gmail.com>
2024-03-01 21:47:36 -0300

    botão janela menu principal

--- LojaABC/frmMenuPrincipal.Designer.cs
+++ LojaABC/frmMenuPrincipal.Designer.cs
@@ -29,13 +29,91 @@ namespace LojaABC
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(217, 233);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Clientes";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(672, 578);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(168, 36);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "VOLTAR";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(327, 44);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(217, 233);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Produtos";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(623, 44);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(217, 233);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "Fornecedores";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(30, 329);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(217, 233);
+            this.button4.TabIndex = 11;
+            this.button4.Text = "Funcionários";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(327, 329);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(217, 233);
+            this.button5.TabIndex = 12;
+            this.button5.Text = "Vendas";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(623, 329);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(217, 233);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "Usuários";
+            this.button6.UseVisualStyleBackColor = true;
             // 
             // frmMenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(871, 614);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Name = "frmMenuPrincipal";
             this.Text = "frmMenuPrincipal";
@@ -44,5 +122,13 @@ namespace LojaABC
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- LojaABC/frmMenuPrincipal.cs
+++ LojaABC/frmMenuPrincipal.cs
@@ -16,5 +16,15 @@ namespace LojaABC
         {
             InitializeComponent();
         }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
